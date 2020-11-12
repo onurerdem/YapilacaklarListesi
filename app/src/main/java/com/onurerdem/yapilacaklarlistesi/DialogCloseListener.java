cf9aad37531ed98bb39336d7894e9fd36ac5771b
Onur Erdem <erdem.onur@outlook.com.tr>
2020-11-12 23:00:16 +0300

    Proje güncellendi.

---
+++ app/src/main/java/com/onurerdem/yapilacaklarlistesi/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.onurerdem.yapilacaklarlistesi;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
--- app/src/main/java/com/onurerdem/yapilacaklarlistesi/MainActivity.java
+++ app/src/main/java/com/onurerdem/yapilacaklarlistesi/MainActivity.java
@@ -1,14 +1,205 @@
 package com.onurerdem.yapilacaklarlistesi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.onurerdem.yapilacaklarlistesi.Adapter.ToDoAdapter;
+import com.onurerdem.yapilacaklarlistesi.Model.ToDoModel;
+import com.onurerdem.yapilacaklarlistesi.Utils.DatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
+
+    private RecyclerView tasksRecyclerview;
+    private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab;
+    private FloatingActionButton fab2;
+
+    private List<ToDoModel> taskList;
+    private DatabaseHandler db;
+
+    SharedPreferences preferences2;
+    SharedPreferences.Editor editor2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferences2 = this.getSharedPreferences("dil", Context.MODE_PRIVATE);
+        Locale locale = new Locale(preferences2.getString("dil",""));
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerview = findViewById(R.id.tasksRecyclerview);
+        tasksRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksAdapter = new ToDoAdapter(db, this);
+        tasksRecyclerview.setAdapter(tasksAdapter);
+
+        fab = findViewById(R.id.fab);
+        fab.setColorFilter(Color.WHITE);
+        fab2 = findViewById(R.id.fab2);
+        fab2.setColorFilter(Color.WHITE);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerview);
+
+        /*ToDoModel task = new ToDoModel();
+        task.setTask((String) getText(R.string.test_task));
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);*/
+
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(),AddNewTask.TAG);
+            }
+        });
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferences2 = getApplicationContext().getSharedPreferences("dil", Context.MODE_PRIVATE);
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.language_settings);
+                Button turkishButton = (Button) dialog.findViewById(R.id.turkishButton);
+                Button englishButton = (Button) dialog.findViewById(R.id.englishButton);
+                Button cancelLanguageButton = (Button) dialog.findViewById(R.id.cancelLanguageButton);
+                TextView languageSettings = (TextView) dialog.findViewById(R.id.language_settings);
+                languageSettings.setText(R.string.languages);
+                turkishButton.setText(R.string.turkish);
+                englishButton.setText(R.string.english);
+                cancelLanguageButton.setText(R.string.cancel);
+                turkishButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        editor2 = preferences2.edit();
+                        editor2.putString("dil","");
+                        editor2.commit();
+                        Locale locale = new Locale(preferences2.getString("dil",""));
+                        Locale.setDefault(locale);
+                        Configuration config = new Configuration();
+                        config.locale = locale;
+                        getBaseContext().getResources().updateConfiguration(config,
+                                getBaseContext().getResources().getDisplayMetrics());
+                        dialog.dismiss();
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                englishButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        editor2 = preferences2.edit();
+                        editor2.putString("dil","en");
+                        editor2.commit();
+                        Locale locale = new Locale(preferences2.getString("dil",""));
+                        Locale.setDefault(locale);
+                        Configuration config = new Configuration();
+                        config.locale = locale;
+                        getBaseContext().getResources().updateConfiguration(config,
+                                getBaseContext().getResources().getDisplayMetrics());
+                        dialog.dismiss();
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                cancelLanguageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.delete_task);
+                builder.setMessage(R.string.delete_task_ask);
+                builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        editor2 = preferences2.edit();
+                        editor2.putString("dil","");
+                        editor2.commit();
+                        Locale locale = new Locale(preferences2.getString("dil",""));
+                        Locale.setDefault(locale);
+                        Configuration config = new Configuration();
+                        config.locale = locale;
+                        getBaseContext().getResources().updateConfiguration(config,
+                                getBaseContext().getResources().getDisplayMetrics());
+                        dialog.dismiss();
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        editor2 = preferences2.edit();
+                        editor2.putString("dil","en");
+                        editor2.commit();
+                        Locale locale = new Locale(preferences2.getString("dil",""));
+                        Locale.setDefault(locale);
+                        Configuration config = new Configuration();
+                        config.locale = locale;
+                        getBaseContext().getResources().updateConfiguration(config,
+                                getBaseContext().getResources().getDisplayMetrics());
+                        dialog.dismiss();
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED);
+                //dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimaryDark));*/
+            }
+        });
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
